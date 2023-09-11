dc10536381010b4a2cffc0cd0ec16cfb45ef11ee
paras-017 <paraschauhan0017@gmail.com>
2023-09-11 20:51:06 +0530

    dictionary

---
+++ basics/10_Dictionary.py
@@ -0,0 +1,14 @@
+dict1 = {}                  #its dictionary not empty set to create empty set you can create set by ==>b = set()
+print(type(dict1))
+
+
+dict2 = {
+    "st_name":"paras",
+    "st_rollNo":33,
+    "st_class":"VII-C",
+}
+print(dict2["st_rollNo"])                #gives error if key is not found
+print(dict2.get("st_rollNo"))            #saves you from getting error if key not found
+
+print(dict2.keys())
+print(dict2.values())
