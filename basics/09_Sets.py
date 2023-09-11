7581be3227fac48860919576984c30b7a352805a
paras-017 <paraschauhan0017@gmail.com>
2023-09-11 20:37:14 +0530

    Sets added

---
+++ basics/09_Sets.py
@@ -0,0 +1,8 @@
+a1 = {3,5,24,5,5,34,122,122,121,1,1,13,3}
+a2 = {40,50,50,60,60,60,70,700,710,1,2,2,4,2,5,2,3,5}
+# a1.clear()
+print(a1.pop())        #removes random number from set
+print(a2)
+
+print(a1.union(a2))                        #addtion of a1,a2
+print(a1.intersection(a2))                #common in a1,a2
