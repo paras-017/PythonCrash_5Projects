6895dcf910db8b1dda81710157098408a366f90d
paras-017 <paraschauhan0017@gmail.com>
2023-09-12 07:29:14 +0530

    ifELse

---
+++ basics/11_if_else.py
@@ -0,0 +1,8 @@
+age = int(input('Enter you age\n'))
+
+if(age>=18):
+    print('Yes, you can drive')
+elif(age == 1):
+    print('You are just borned. ENjoy you fucking life')
+else:
+    print('No, you can\' drive')
