cb6ead2d56333eb48bb215239a73d3311183d1cd
paras-017 <paraschauhan0017@gmail.com>
2023-09-12 10:50:52 +0530

    for loop

--- basics/12_match_case.py
+++ basics/12_match_case.py
@@ -1,17 +1,22 @@
-a = int(input("Enter the Number"))
-match a:
-    case 1:
-        print("Rating is one")
-    case 2:
-        print("Rating is on2")
-    case 3:
-        print("Rating is 3")
-    case 4:
-        print("Rating is 4")
-    case 5:
-        print("Rating is 5")
-    case _:
-        print("Default is running")
+# a = int(input("Enter the Number"))
+# match a:
+#     case 1:
+#         print("Rating is one")
+#     case 2:
+#         print("Rating is on2")
+#     case 3:
+#         print("Rating is 3")
+#     case 4:
+#         print("Rating is 4")
+#     case 5:
+#         print("Rating is 5")
+#     case _:
+#         print("Default is running")
 
 
 # Quick QUIZ : write a python program to print table of a number which liews between 1 and 10
+num = int(input("Enter the number you want to find table"))
+match num:
+    case 5:
+        for i in range(5,5*10+1,5):
+            print(f"5 x {int(i/5)} = {i}")
