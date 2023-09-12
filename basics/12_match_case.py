a5f1b014e0ccbb3ab622e787567c60943ca3c703
paras-017 <paraschauhan0017@gmail.com>
2023-09-12 10:39:20 +0530

    match casees

---
+++ basics/12_match_case.py
@@ -0,0 +1,17 @@
+a = int(input("Enter the Number"))
+match a:
+    case 1:
+        print("Rating is one")
+    case 2:
+        print("Rating is on2")
+    case 3:
+        print("Rating is 3")
+    case 4:
+        print("Rating is 4")
+    case 5:
+        print("Rating is 5")
+    case _:
+        print("Default is running")
+
+
+# Quick QUIZ : write a python program to print table of a number which liews between 1 and 10
