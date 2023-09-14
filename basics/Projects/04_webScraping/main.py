665fbb7a89ffab4231256ab96388da98f4b428b8
paras-017 <paraschauhan0017@gmail.com>
2023-09-14 10:45:26 +0530

    retriving data

--- basics/Projects/04_webScraping/main.py
+++ basics/Projects/04_webScraping/main.py
@@ -1,8 +1,8 @@
 import requests
-url = 'https://www.youtube.com/playlist?list=PLt9sXaOeDEtOrmGppdXLQ6jZBNVLDK2SX'
+url = "https://www.python.org/"
 
 def fetchandSaveFile(url, path):
-    r = requests.get(url)
+    r = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"})
     with open(path, 'w') as f:
         f.write(r.text)
 
